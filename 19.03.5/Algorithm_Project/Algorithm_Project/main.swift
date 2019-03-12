c28ccccbba1cfbb7f0df478c9c2377e77dcb6c4c
BaeJiho <hcw53590@gmail.com>
2019-03-12 19:34:56 +0900

    ing..

--- 19.03.5/Algorithm_Project/Algorithm_Project/main.swift
+++ 19.03.5/Algorithm_Project/Algorithm_Project/main.swift
@@ -203,7 +203,7 @@ default:
  
  상근이가 설탕을 정확하게 N킬로그램 배달해야 할 때, 봉지 몇 개를 가져가면 되는지 그 수를 구하는 프로그램을 작성하시오.
  */
-
+/*
 let line1 = readLine() ?? ""
 let input = Int(line1) ?? 0
 
@@ -222,4 +222,69 @@ while result >= 0 {
         print(-1)
     }
 }
+*/
+//0보다 크거나 같고, 99보다 작거나 같은 정수가 주어질 때 다음과 같은 연산을 할 수 있다. 먼저 주어진 수가 10보다 작다면 앞에 0을 붙여 두 자리 수로 만들고, 각 자리의 숫자를 더한다. 그 다음, 주어진 수의 가장 오른쪽 자리 수와 앞에서 구한 합의 가장 오른쪽 자리 수를 이어 붙이면 새로운 수를 만들 수 있다. 다음 예를 보자.
+//26부터 시작한다. 2+6 = 8이다. 새로운 수는 68이다. 6+8 = 14이다. 새로운 수는 84이다. 8+4 = 12이다. 새로운 수는 42이다. 4+2 = 6이다. 새로운 수는 26이다.
+//위의 예는 4번만에 원래 수로 돌아올 수 있다. 따라서 26의 사이클의 길이는 4이다.
+//N이 주어졌을 때, N의 사이클의 길이를 구하는 프로그램을 작성하시오.
+
+let line1 = readLine() ?? ""
+
+let n = Int(line1) ?? 0
+var data = String(n)
+
+var li: [Int] = []
+var count = 0
+
+
+for i in data.indices {
+    let s = data[i]
+    if let data = Int(String(s)) {
+        li.append(data)
+    }
+}
+if li.count >= 3 {
+    print("error")
+} else if li.count == 1 {
+    li.insert(0, at: 0)
+}
+print(li)
+var first = li[0]
+var second = li[1]
+var total = 0
+
+total = first + second
+
+
+
+
+
+
+
+
+
+
+//let line1 = Array(readLine() ?? "")
+//var list = line1
+//var s: [Int] = []
+//if list.count >= 3 {
+//    print("error")
+//}
+//if list.count == 1 {
+//    if list[0] == "0" {
+//        print("error")
+//    } else {
+//        list.insert("0", at: 0)
+//    }
+//
+//}
+//for i in list {
+//    if let data = Int(String(i)) {
+//        s.append(data)
+//    }
+//}
+
+//let input = line1.split(separator: " ").map(String.init)
+//let first = Int(input[0]) ?? 0
+//let second = Int(input[1]) ?? 0
 
